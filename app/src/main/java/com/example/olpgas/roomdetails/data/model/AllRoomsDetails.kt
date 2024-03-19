89689940f34f1ba071ad64c95240853a1b4920f1
Nizam Chaudhary <ChaudharyNizam10@gmail.com>
2024-03-19 16:36:11 +0530

    removed of fetching of images at same time.
    Signed-off-by: Nizam Chaudhary <ChaudharyNizam10@gmail.com>

--- app/src/main/java/com/example/olpgas/roomdetails/data/model/AllRoomsDetails.kt
+++ app/src/main/java/com/example/olpgas/roomdetails/data/model/AllRoomsDetails.kt
@@ -1,20 +1,18 @@
 package com.example.olpgas.roomdetails.data.model
 
 import kotlinx.serialization.Serializable
-import java.util.UUID
 
 @Serializable
 data class AllRoomsDetails(
     val id: Int,
     val ownerId: String,
     val roomName: String,
-    val roomNumber: String,
+    var roomNumber: String,
     val roomFeatureId: Int,
     val roomType: String,
     val rentAmount: Int,
     val deposit: Int,
     val city: String,
-    val state: String,
-    val images: MutableList<ByteArray>? = null
+    val state: String
 )
 
--- app/src/main/java/com/example/olpgas/roomdetails/viewmodel/RoomsViewModel.kt
+++ app/src/main/java/com/example/olpgas/roomdetails/viewmodel/RoomsViewModel.kt
@@ -5,13 +5,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.olpgas.auth.data.network.SupabaseClient
 import com.example.olpgas.auth.data.network.SupabaseClient.client
 import com.example.olpgas.roomdetails.data.model.AllRoomsDetails
 import io.github.jan.supabase.postgrest.postgrest
-import io.github.jan.supabase.storage.storage
 import kotlinx.coroutines.launch
-import kotlin.reflect.jvm.internal.impl.descriptors.deserialization.PlatformDependentDeclarationFilter.All
 
 class RoomsViewModel : ViewModel() {
 
@@ -24,21 +21,16 @@ class RoomsViewModel : ViewModel() {
                 val allRoomsDetails = client.postgrest.from("AllRooms")
                     .select().decodeList<AllRoomsDetails>()
 
-                for(room in allRoomsDetails) {
-                    val bucket = client.storage.from("RoomPics")
-                    val mainBytes = bucket.downloadPublic("${room.ownerId}/main.jpg")
-                    room.images?.add(mainBytes)
-
-                    for(i in 1..3) {
-                        val subBytes = bucket.downloadPublic("${room.ownerId}/sub$i.jpg")
-                        room.images?.add(subBytes)
-                    }
-                }
-
                 _allRoomsDetails.value = allRoomsDetails
             } catch(e: Exception) {
                 Log.e("Room","Error: ${e.message}")
             }
+        }
+    }
+
+    fun fetchDisplayImages() {
+        viewModelScope.launch {
+
         }
     }
 }
