83f6031d2d3c024ff7a8246fe3ad4c6e5e070850
Nizam Chaudhary <chaudharynizam10@gmail.com>
2024-09-10 11:24:13 +0530

    chore: update plugins
    Signed-off-by: Nizam Chaudhary <ChaudharyNizam10@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,27 +48,27 @@ kapt {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
-    implementation("com.google.android.gms:play-services-pal:20.2.0")
+    implementation("com.google.android.gms:play-services-pal:20.3.0")
     implementation("androidx.hilt:hilt-work:1.2.0")
     implementation("androidx.activity:activity:1.8.2")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
 
     // ViewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
 
     // Splash Screen
     implementation("androidx.core:core-splashscreen:1.0.1")
@@ -83,9 +83,9 @@ dependencies {
     implementation("io.ktor:ktor-client-android:2.3.9")
 
     // Google Credential Manager
-    implementation("androidx.credentials:credentials:1.2.1")
-    implementation("androidx.credentials:credentials-play-services-auth:1.2.1")
-    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.0")
+    implementation("androidx.credentials:credentials:1.2.2")
+    implementation("androidx.credentials:credentials-play-services-auth:1.2.2")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
 
     // Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
@@ -106,6 +106,9 @@ dependencies {
     implementation("com.tbuonomo:dotsindicator:5.0")
 
     // WorkManager
-    implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation("androidx.work:work-runtime-ktx:2.9.1")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
 }
 
